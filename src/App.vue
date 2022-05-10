36fa3c964fff0863d0814b6dcb77ffd2f9634f73
nishajame <nishajames.claysys@gmail.com>
2022-05-10 15:28:51 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <button @click="logout">Logout</button>
+  <p v-font-size:small>Small</p>
+  <p v-font-size:Medium>Medium</p>
+  <p v-font-size:Large>Large</p>
+  <div>
+    <MyHeader/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import { inject } from "vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld
+  },
+  setup() {
+    const logout = inject("logout");
+
+    return {
+      logout,
+    };
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import MyFirstPlugin from 'plugin-nisha123'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(MyFirstPlugin, {
+    fontSize:{
+        small: 12,
+        medium: 24,
+        large: 36
+    }
+})
+
+app.mount('#app')
